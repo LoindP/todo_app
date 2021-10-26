a7ce2ece97eef17badacaf03b0a97ec16f1dc67a
Aleksander <sanyars2199@gmail.com>
2021-10-26 22:16:53 +0300

    fix bug in component app(filter: "done" in state)

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -17,7 +17,7 @@ export default class App extends Component{
             this.createTodoItem('Have a lunch'),
         ],
         term: '',
-        filter: 'DONE',
+        filter: 'ALL',
     }
 
     createTodoItem(label){
